c8895fc188e5e9e2675c4feeac6ebf0437f43c0e
Alexis Ligon <aligon17@gmail.com>
2021-05-09 13:34:56 -0400

    deleted old api, using new api, ratatouille recipe

--- recipe.js
+++ recipe.js
@@ -1,83 +1,44 @@
-var apiKey = '9026ee9d63e841e98119c51b35cff2ec';
+//RESTART WITH NEW API
 
-//search by cuisine
-fetch(`https://api.spoonacular.com/recipes/complexSearch?cuisine=italian&apiKey=${apiKey}`)
-.then(function(response) {
-    return response.json();
-})
-.then(function(data){
-    console.log('data: ', data);
-})
-
-
-
-//FRENCH CUISINE TITLE AND INSTRUCTIONS
+//RATATOUILLE RECIPE
 $.ajax({
-    url: `https://api.spoonacular.com/recipes/633754/information?apiKey=${apiKey}`,
-    method: 'GET'
-}) .then(function(response){
-    console.log('FRENCH CUISINE!!!!!!!!!!!');
-    console.log(response.title);
-    console.log(response.instructions);
+	url: 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52908',//ingredients and recipe details by id
+	method: 'GET'
+}).then(function(data){
+	console.log('DATA!!!!!!!!', data);
+	console.log('TITLE: ', data.meals[0].strMeal);
+	console.log('INGREDIENTS')
+
+	console.log(data.meals[0].strMeasure1);
+	console.log(data.meals[0].strIngredient1);
+
+	console.log(data.meals[0].strMeasure2);
+	console.log(data.meals[0].strIngredient2);
+
+	console.log(data.meals[0].strMeasure3);
+	console.log(data.meals[0].strIngredient3);
+
+	console.log(data.meals[0].strMeasure4);
+	console.log(data.meals[0].strIngredient4);
+
+	console.log(data.meals[0].strMeasure5);
+	console.log(data.meals[0].strIngredient5);
+
+	console.log(data.meals[0].strMeasure6);
+	console.log(data.meals[0].strIngredient6);
+
+	console.log(data.meals[0].strMeasure7);
+	console.log(data.meals[0].strIngredient7);
+
+	console.log(data.meals[0].strMeasure8);
+	console.log(data.meals[0].strIngredient8);
+
+	console.log(data.meals[0].strMeasure9);
+	console.log(data.meals[0].strIngredient9);
+
+	console.log(data.meals[0].strMeasure10);
+	console.log(data.meals[0].strIngredient10);
+
+	console.log('RECIPE INSTRUCTIONS!!!!!');
+	console.log(data.meals[0].strInstructions)
 })
-
-//FRENCH CUISINE INGREDIENTS LIST
-$.ajax({
-    url: `https://api.spoonacular.com/recipes/633754/ingredientWidget.json?apiKey=${apiKey}`,
-    method: 'GET'
-}) .then(function(response){
-    console.log('FRENCH CUISINE INGREDIENTS!!!!!!!!!')
-    for(i=0; i<response.ingredients.length; i++){
-        console.log(response.ingredients[i].amount.us.value);
-        console.log(response.ingredients[i].amount.us.unit);
-        console.log(response.ingredients[i].name);
-    }
-
-})
-
-//MEXICAN CUISINE TITLE AND INSTRUCTIONS
-$.ajax({
-    url: `https://api.spoonacular.com/recipes/640117/information?apiKey=${apiKey}`,
-    method: 'GET'
-}).then(function(response){
-    console.log('MEXICAN CUISINE!!!!!!!!!');
-    console.log(response.title);
-    console.log(response.instructions);
-})
-
-//MEXICAN CUISINE INGREDIENTS LIST
-$.ajax({
-    url: `https://api.spoonacular.com/recipes/640117/ingredientWidget.json?apiKey=${apiKey}`,
-    method: 'GET'
-}) .then(function(response){
-    console.log('MEXICAN CUISINE INGREDIENTS!!!!!!!!');
-    for(i=0; i<response.ingredients.length; i++){
-        console.log(response.ingredients[i].amount.us.value);
-        console.log(response.ingredients[i].amount.us.unit);
-        console.log(response.ingredients[i].name);
-    }
-})
-
-//ITALIAN CUISINE TITLE AND INSTRUCTIONS
-$.ajax({
-    url: `https://api.spoonacular.com/recipes/715573/information?apiKey=${apiKey}`,
-    method: 'GET'
-}).then(function(response){
-    console.log('ITALIAN CUISINE!!!!!!');
-    console.log(response.title);
-    console.log(response.instructions);
-})
-
-//ITALIAN CUISINE INGREDIENTS LIST
-$.ajax({
-    url: `https://api.spoonacular.com/recipes/715573/ingredientWidget.json?apiKey=${apiKey}`,
-    method: 'GET'
-}) .then(function(response){
-    console.log('ITALIAN CUISINE INGREDIENTS!!!!!!!');
-    for(i=0; i<response.ingredients.length; i++){
-        console.log(response.ingredients[i].amount.us.value);
-        console.log(response.ingredients[i].amount.us.unit);
-        console.log(response.ingredients[i].name);
-    }
-})
-
