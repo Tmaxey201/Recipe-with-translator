972979a6dc17727b6a0d20687932fdbbf9170d01
Alexis Ligon <aligon17@gmail.com>
2021-05-09 13:38:49 -0400

    chicken enchilada casserole recipe

--- recipe.js
+++ recipe.js
@@ -42,3 +42,29 @@ $.ajax({
 	console.log('RECIPE INSTRUCTIONS!!!!!');
 	console.log(data.meals[0].strInstructions)
 })
+
+//CHICKEN ENCHILADA CASSEROLE
+$.ajax({
+	url: 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52908',//ingredients and recipe details by id
+	method: 'GET'
+}).then(function(data){
+	console.log('DATA!!!!!!!!', data);
+	console.log('TITLE: ', data.meals[0].strMeal);
+	console.log('INGREDIENTS')
+
+	console.log(data.meals[0].strMeasure1);
+	console.log(data.meals[0].strIngredient1);
+
+	console.log(data.meals[0].strMeasure2);
+	console.log(data.meals[0].strIngredient2);
+
+	console.log(data.meals[0].strMeasure3);
+	console.log(data.meals[0].strIngredient3);
+
+	console.log(data.meals[0].strMeasure4);
+	console.log(data.meals[0].strIngredient4);
+
+
+	console.log('RECIPE INSTRUCTIONS!!!!!');
+	console.log(data.meals[0].strInstructions)
+})
