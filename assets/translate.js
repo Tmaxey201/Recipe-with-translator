ace56688b11148bd18037daa37046d68a7b53a1c
Thomas Maxey <tmaxey201@gmail.com>
2021-05-09 14:29:58 -0400

    fixed card layout

--- assets/translate.js
+++ assets/translate.js
@@ -2,16 +2,16 @@ $("#translateButton").click(function () {
     
 	var url = "https://translation.googleapis.com/language/translate/v2";
 	//Strings requiring translation
-	url += "?q=" + escape($("#textField").text());
-	url += "&q=" + escape($("#title").text());
+	url += "?q=" + escape($(".textField").text());
+	url += "&q=" + escape($(".textlang").text());
 	//Target language
 	url += "&target=" + $("#targetLanguage").val();
 	//Replace with your API key
 	url += "&key=AIzaSyDRxT8PDxPBZlHDS6jq6pebU_Jqz5reBQ8";
 	$.get(url, function (data, status) {
 		//Results are returned in an array following the order they were passed. 
-		$("#textField").text(data.data.translations[0].translatedText);
-		$("#title").text(data.data.translations[1].translatedText);
+		$(".textField").text(data.data.translations[0].translatedText);
+		$(".textlang").text(data.data.translations[1].translatedText);
 	});       
 });
 
