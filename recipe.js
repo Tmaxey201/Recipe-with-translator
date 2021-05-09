60fc2d922fcd7e448b100b99442d311501f53dae
Alexis Ligon <aligon17@gmail.com>
2021-05-09 13:54:15 -0400

    lasagna recipe

--- recipe.js
+++ recipe.js
@@ -45,7 +45,7 @@ $.ajax({
 
 //CHICKEN ENCHILADA CASSEROLE
 $.ajax({
-	url: 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52908',//ingredients and recipe details by id
+	url: 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52765',//ingredients and recipe details by id
 	method: 'GET'
 }).then(function(data){
 	console.log('DATA!!!!!!!!', data);
@@ -64,6 +64,64 @@ $.ajax({
 	console.log(data.meals[0].strMeasure4);
 	console.log(data.meals[0].strIngredient4);
 
+
+	console.log('RECIPE INSTRUCTIONS!!!!!');
+	console.log(data.meals[0].strInstructions)
+})
+
+//LASAGNE RECIPE
+$.ajax({
+	url: 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52844',//ingredients and recipe details by id
+	method: 'GET'
+}).then(function(data){
+	console.log('DATA!!!!!!!!', data);
+	console.log('TITLE: ', data.meals[0].strMeal);
+	console.log('INGREDIENTS')
+
+	console.log(data.meals[0].strMeasure1);
+	console.log(data.meals[0].strIngredient1);
+
+	console.log(data.meals[0].strMeasure2);
+	console.log(data.meals[0].strIngredient2);
+
+	console.log(data.meals[0].strMeasure3);
+	console.log(data.meals[0].strIngredient3);
+
+	console.log(data.meals[0].strMeasure4);
+	console.log(data.meals[0].strIngredient4);
+
+	console.log(data.meals[0].strMeasure5);
+	console.log(data.meals[0].strIngredient5);
+
+	console.log(data.meals[0].strMeasure6);
+	console.log(data.meals[0].strIngredient6);
+
+	console.log(data.meals[0].strMeasure7);
+	console.log(data.meals[0].strIngredient7);
+
+	console.log(data.meals[0].strMeasure8);
+	console.log(data.meals[0].strIngredient8);
+
+	console.log(data.meals[0].strMeasure9);
+	console.log(data.meals[0].strIngredient9);
+
+	console.log(data.meals[0].strMeasure10);
+	console.log(data.meals[0].strIngredient10);
+
+    console.log(data.meals[0].strMeasure11);
+	console.log(data.meals[0].strIngredient11);
+
+    console.log(data.meals[0].strMeasure12);
+	console.log(data.meals[0].strIngredient12);
+
+    console.log(data.meals[0].strMeasure13);
+	console.log(data.meals[0].strIngredient13);
+
+    console.log(data.meals[0].strMeasure14);
+	console.log(data.meals[0].strIngredient14);
+
+    console.log(data.meals[0].strMeasure15);
+	console.log(data.meals[0].strIngredient15);
 
 	console.log('RECIPE INSTRUCTIONS!!!!!');
 	console.log(data.meals[0].strInstructions)
