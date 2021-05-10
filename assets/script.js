90f3b64587404f51bf8aa1f7ffb3eb500385ffc2
Thomas Maxey <tmaxey201@gmail.com>
2021-05-10 17:09:27 -0400

    added header with language icons and a toggle switch

--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,25 @@ $(document).ready(function(){
     $('.parallax').parallax();
   });
 
-$(".dropdown-trigger").dropdown();
-
-
 
 function yourfunction(){
   console.log("hello world")
 }
 
+
+
+// jquery for switch to toggle language nav
+$(document).ready(function(){
+  $('select').formSelect();
+});
+
+$(document).on('change', '#check', function(e){
+    if($(this).is(':checked'))
+{
+  $(".hidenav").show();
+}
+else
+{
+  $(".hidenav").hide(); 
+}
+})
