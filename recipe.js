bef39d9e0edb0feaee089acdb947c2710537cc02
Alexis Ligon <aligon17@gmail.com>
2021-05-10 11:21:24 -0400

    append lasagna recipe to webpage

--- recipe.js
+++ recipe.js
@@ -32,26 +32,6 @@ $.ajax({
 	$('#ingredients-list ul').append(`<li>${data.meals[0].strMeasure2} ${data.meals[0].strIngredient2}`);
 	$('#ingredients-list ul').append(`<li>${data.meals[0].strMeasure3} ${data.meals[0].strIngredient3}`);
 	$('#ingredients-list ul').append(`<li>${data.meals[0].strMeasure4} ${data.meals[0].strIngredient4}`);
-
-	// console.log('DATA!!!!!!!!', data);
-	// console.log('TITLE: ', data.meals[0].strMeal);
-	// console.log('INGREDIENTS')
-
-	// console.log(data.meals[0].strMeasure1);
-	// console.log(data.meals[0].strIngredient1);
-
-	// console.log(data.meals[0].strMeasure2);
-	// console.log(data.meals[0].strIngredient2);
-
-	// console.log(data.meals[0].strMeasure3);
-	// console.log(data.meals[0].strIngredient3);
-
-	// console.log(data.meals[0].strMeasure4);
-	// console.log(data.meals[0].strIngredient4);
-
-
-	// console.log('RECIPE INSTRUCTIONS!!!!!');
-	// console.log(data.meals[0].strInstructions)
 })
 
 //LASAGNE RECIPE - ITALY
@@ -59,57 +39,24 @@ $.ajax({
 	url: 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52844',
     method: 'GET'
 }).then(function(data){
-	console.log('DATA!!!!!!!!', data);
-	console.log('TITLE: ', data.meals[0].strMeal);
-	console.log('INGREDIENTS')
 
-	console.log(data.meals[0].strMeasure1);
-	console.log(data.meals[0].strIngredient1);
-
-	console.log(data.meals[0].strMeasure2);
-	console.log(data.meals[0].strIngredient2);
-
-	console.log(data.meals[0].strMeasure3);
-	console.log(data.meals[0].strIngredient3);
-
-	console.log(data.meals[0].strMeasure4);
-	console.log(data.meals[0].strIngredient4);
-
-	console.log(data.meals[0].strMeasure5);
-	console.log(data.meals[0].strIngredient5);
-
-	console.log(data.meals[0].strMeasure6);
-	console.log(data.meals[0].strIngredient6);
-
-	console.log(data.meals[0].strMeasure7);
-	console.log(data.meals[0].strIngredient7);
-
-	console.log(data.meals[0].strMeasure8);
-	console.log(data.meals[0].strIngredient8);
-
-	console.log(data.meals[0].strMeasure9);
-	console.log(data.meals[0].strIngredient9);
-
-	console.log(data.meals[0].strMeasure10);
-	console.log(data.meals[0].strIngredient10);
-
-    console.log(data.meals[0].strMeasure11);
-	console.log(data.meals[0].strIngredient11);
-
-    console.log(data.meals[0].strMeasure12);
-	console.log(data.meals[0].strIngredient12);
-
-    console.log(data.meals[0].strMeasure13);
-	console.log(data.meals[0].strIngredient13);
-
-    console.log(data.meals[0].strMeasure14);
-	console.log(data.meals[0].strIngredient14);
-
-    console.log(data.meals[0].strMeasure15);
-	console.log(data.meals[0].strIngredient15);
-
-	console.log('RECIPE INSTRUCTIONS!!!!!');
-	console.log(data.meals[0].strInstructions)
+	$('#recipe-title').text(data.meals[0].strMeal);
+	$('#instructions').text(data.meals[0].strInstructions);
+	$('#ingredients-list ul').append(`<li>${data.meals[0].strMeasure1} ${data.meals[0].strIngredient1}`)
+	$('#ingredients-list ul').append(`<li>${data.meals[0].strMeasure2} ${data.meals[0].strIngredient2}`);
+	$('#ingredients-list ul').append(`<li>${data.meals[0].strMeasure3} ${data.meals[0].strIngredient3}`);
+	$('#ingredients-list ul').append(`<li>${data.meals[0].strMeasure4} ${data.meals[0].strIngredient4}`);
+	$('#ingredients-list ul').append(`<li>${data.meals[0].strMeasure5} ${data.meals[0].strIngredient5}`);
+	$('#ingredients-list ul').append(`<li>${data.meals[0].strMeasure6} ${data.meals[0].strIngredient6}`);
+	$('#ingredients-list ul').append(`<li>${data.meals[0].strMeasure7} ${data.meals[0].strIngredient7}`);
+	$('#ingredients-list ul').append(`<li>${data.meals[0].strMeasure8} ${data.meals[0].strIngredient8}`);
+	$('#ingredients-list ul').append(`<li>${data.meals[0].strMeasure9} ${data.meals[0].strIngredient9}`);
+	$('#ingredients-list ul').append(`<li>${data.meals[0].strMeasure10} ${data.meals[0].strIngredient10}`);
+	$('#ingredients-list ul').append(`<li>${data.meals[0].strMeasure11} ${data.meals[0].strIngredient11}`);
+	$('#ingredients-list ul').append(`<li>${data.meals[0].strMeasure12} ${data.meals[0].strIngredient12}`);
+	$('#ingredients-list ul').append(`<li>${data.meals[0].strMeasure13} ${data.meals[0].strIngredient13}`);
+	$('#ingredients-list ul').append(`<li>${data.meals[0].strMeasure14} ${data.meals[0].strIngredient14}`);
+	$('#ingredients-list ul').append(`<li>${data.meals[0].strMeasure15} ${data.meals[0].strIngredient15}`);
 })
 
 //YAKI UDON RECIPE - JAPAN
